2713b621b6168592781ef05b670f600f4e3683f8
Chingsan53 <cly53@csu.fullerton.edu>
2025-02-06 10:47:30 -0800

    finished 2nd exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function (word, value) {
+  let string = "";
+  if (value < 0) return "ERROR";
+  for (let rp = 0; rp < value; rp++) string += word;
+  return string;
 };
 
 // Do not edit below this line
