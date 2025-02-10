b9ad3475ba675c28b122c796e8730e5117d75405
Chingsan53 <cly53@csu.fullerton.edu>
2025-02-10 11:22:44 -0800

    did 3rd exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function (words) {
+  let newWord = "";
+  for (let i = words.length - 1; i >= 0; i--) {
+    newWord += words[i];
+  }
 
+  return newWord;
 };
 
 // Do not edit below this line
